878a1df386146500a4e3edb67ecbfedb59a35f95
Hwidong Bae <spilist@gmail.com>
2015-08-21 17:30:40 +0900

    ObjectsMenu work in progress

---
+++ assets/Scripts/05_Menus/ObjectsMenu/UIObjects.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIObjects : MonoBehaviour {
+  public string objectName;
+  public string description;
+  public int price;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
