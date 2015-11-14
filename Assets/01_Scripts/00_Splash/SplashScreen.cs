4629908a92303ccdf9aba65a26ce1247ecc40b01
Hwidong Bae <spilist@gmail.com>
2015-11-14 18:07:08 +0900

    스플래시 스크린

--- Assets/01_Scripts/00_Splash/SplashScreen.cs
+++ Assets/01_Scripts/00_Splash/SplashScreen.cs
@@ -4,21 +4,20 @@ using UnityEngine.UI;
 
 public class SplashScreen : MonoBehaviour {
 	public float splashDuring = 3f;
-	private CanvasRenderer cr;
-	private bool alphaIncreasing = true;
-
-	void Start () {
-		cr = GetComponent<Image>().canvasRenderer;
-		cr.SetAlpha( 0.0f );
-	}
+	private float count;
+	private bool loaded = false;
 
 	void Update () {
-		if (alphaIncreasing) {
-			cr.SetAlpha(Mathf.MoveTowards(cr.GetAlpha(), 1, Time.deltaTime * 2 / splashDuring));
-			if (cr.GetAlpha() == 1) alphaIncreasing = false;
-		} else {
-			cr.SetAlpha(Mathf.MoveTowards(cr.GetAlpha(), 0, Time.deltaTime * 2 / splashDuring));
-			if (cr.GetAlpha() == 0) Application.LoadLevel("2_BeforeMainScene");
+		if (count < splashDuring) count += Time.deltaTime;
+		else {
+			if (!loaded) {
+				loaded = true;
+				if (DataManager.dm.getBool("TutorialDone")) {
+				  Application.LoadLevelAsync("2_BeforeMainScene");
+				} else {
+				  Application.LoadLevelAsync("_Tutorial");
+				}
+			}
 		}
 	}
 }
