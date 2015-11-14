1e67a9d163cae1eb0edff09733de569fb993a354
Hwidong Bae <spilist@gmail.com>
2015-11-14 14:30:14 +0900

    Opening 끝남

---
+++ Assets/01_Scripts/02_BeforeMain/OpeningCandy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningCandy : MonoBehaviour {
+  public OpeningHandler handler;
+  public float startScale = 0.1f;
+  public float stayScale = 1;
+  public float smallScale = 0.6f;
+  public float largeScale = 1.2f;
+
+  public float changeTime = 0.1f;
+  public GameObject next;
+
+  private float scale;
+  private int status = 0;
+
+  void OnEnable() {
+    transform.localScale = startScale * Vector3.one;
+    scale = startScale;
+    status++;
+  }
+
+  void Update() {
+    if (status > 0) {
+      if (status == 1) {
+        changeScale(largeScale, largeScale - startScale);
+      } else if (status == 2) {
+        changeScale(smallScale, smallScale - largeScale);
+      } else if (status == 3) {
+        changeScale(stayScale, stayScale - smallScale);
+      } else if (status == 4) {
+        if (next != null) next.SetActive(true);
+        else handler.moveDown();
+        status++;
+      }
+
+      transform.localScale = scale * Vector3.one;
+    }
+  }
+
+  void changeScale(float targetScale, float difference) {
+    scale = Mathf.MoveTowards(scale, targetScale, Time.deltaTime * Mathf.Abs(difference) / changeTime);
+    if (scale == targetScale) status++;
+  }
+}
---
+++ Assets/01_Scripts/02_BeforeMain/OpeningHandler.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OpeningHandler : MonoBehaviour {
+  public GameObject firstCandy;
+  public float cameraMoveTo = 200;
+  public float movingIn = 0.4f;
+  public float stayBeforeMove = 1;
+  private float stayCount;
+  private bool movingDown = false;
+  private float cameraZ;
+
+  public Text copyright;
+  private Color copyrightColor;
+  public float copyrightShowAfter = 0.3f;
+  public float copyrightAlphaChangeDuration = 0.2f;
+  private float copyrightHideCount = 0;
+
+  public GameObject title;
+  public float movingDuration = 0.6f;
+  private bool titleMoving = false;
+  private float titlePosX;
+  private float distance;
+
+	void Start () {
+    cameraZ = 0;
+    firstCandy.SetActive(true);
+
+    titlePosX = title.GetComponent<RectTransform>().anchoredPosition.x;
+    copyrightColor = new Color(1, 1, 1, 0);
+    distance = Mathf.Abs(titlePosX);
+	}
+
+  public void moveDown() {
+    if (!movingDown) movingDown = true;
+  }
+
+  void Update() {
+    if (movingDown) {
+      if (stayCount < stayBeforeMove) stayCount += Time.deltaTime;
+      else {
+        cameraZ = Mathf.MoveTowards(cameraZ, cameraMoveTo, Time.deltaTime * cameraMoveTo / movingIn);
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, cameraZ);
+        if (cameraZ == cameraMoveTo) {
+          movingDown = false;
+          titleMoving = true;
+        }
+      }
+    }
+
+    if (titleMoving) {
+      titlePosX = Mathf.MoveTowards(titlePosX, 0, Time.deltaTime / movingDuration * distance);
+      title.GetComponent<RectTransform>().anchoredPosition = new Vector2(titlePosX, title.GetComponent<RectTransform>().anchoredPosition.y);
+
+      if (copyrightHideCount < copyrightShowAfter) {
+        copyrightHideCount += Time.deltaTime;
+      } else {
+        copyrightColor.a = Mathf.MoveTowards(copyrightColor.a, 1, Time.deltaTime / copyrightAlphaChangeDuration);
+        copyright.color = copyrightColor;
+      }
+
+      if (titlePosX == 0) {
+        titleMoving = false;
+        Application.LoadLevelAsync("_Tutorial");
+      }
+    }
+  }
+}
