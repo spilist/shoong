2ef9d12116271fc21d01ea57b876ba3c8a4f381a
hojang pyun <kyackk@gmail.com>
2016-04-16 15:57:00 +0900

    Heal effect 개선

---
+++ Assets/01_Scripts/20_InGame/Others/FixedRotator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotator : MonoBehaviour {
+  public int speed = 300;
+  public bool clockwise = true;
+  public bool shrinking = true;
+  public float shrinkingDuration = 0.5f;
+
+  Vector3 rotation;
+  float scale;
+
+	void OnEnable() {
+    if (clockwise) rotation = new Vector3(0, 1, 0);
+    else rotation = new Vector3(0, -1, 0);
+
+    scale = 1;
+    transform.localScale = Vector3.one;
+  }
+
+  void Update () {
+    transform.Rotate(rotation * Time.deltaTime * speed);
+
+    if (shrinking) {
+      scale = Mathf.MoveTowards(scale, 0, Time.deltaTime / shrinkingDuration);
+      transform.localScale = scale * Vector3.one;
+    }
+	}
+}
