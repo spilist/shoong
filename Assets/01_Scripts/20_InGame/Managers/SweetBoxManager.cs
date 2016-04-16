53924fc4c21e883f8835073077e8cde7a7c297a3
hojang pyun <kyackk@gmail.com>
2016-04-16 18:39:03 +0900

    Sweetbox 구현

---
+++ Assets/01_Scripts/20_InGame/Managers/SweetBoxManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SweetBoxManager : ObjectsManager {
+  public float generateAfter = 0.4f;
+  public float generatingDuration = 1.1f;
+  public int numGeneration = 50;
+  public float spawnRadius = 200;
+
+	override public void initRest() {
+    runImmediately();
+  }
+
+  override protected void spawn() {
+    if (player == null || ScoreManager.sm.isGameOver()) return;
+
+    Vector2 screenPos = Random.insideUnitCircle;
+    screenPos.Normalize();
+    screenPos *= spawnRadius;
+    Vector3 spawnPos = new Vector3(screenPos.x + player.transform.position.x, player.transform.position.y, screenPos.y + player.transform.position.z);
+
+    instance = getPooledObj(objPool, objPrefab, spawnPos);
+    instance.SetActive(true);
+  }
+}
---
+++ Assets/01_Scripts/20_InGame/Movers/SweetBoxMover.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SweetBoxMover : ObjectsMover {
+  SweetBoxManager sbm;
+  float generateInterval;
+
+	protected override void initializeRest() {
+    sbm = (SweetBoxManager)objectsManager;
+    generateInterval = sbm.generatingDuration / sbm.numGeneration;
+  }
+
+  override protected void afterEnable() {
+  }
+
+  override public string getManager() {
+    return "SweetBoxManager";
+  }
+
+  override protected float getSpeed() {
+    return 0;
+  }
+
+  override protected void afterEncounter() {
+    sbm.GetComponent<NormalPartsManager>().popSweetsByBox(sbm.numGeneration, sbm.generateAfter, generateInterval, transform.position);
+    sbm.run();
+  }
+
+  override public void destroyObject(bool destroyEffect = true, bool byPlayer = false, bool respawn = true) {
+
+    gameObject.SetActive(false);
+
+    if (destroyEffect) {
+      showDestroyEffect(byPlayer);
+    }
+
+    if (byPlayer) {
+      sbm.run();
+    } else {
+      sbm.runImmediately();
+    }
+  }
+}
