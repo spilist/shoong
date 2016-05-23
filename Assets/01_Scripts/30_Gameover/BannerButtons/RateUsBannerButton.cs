1b4749064d3a62d5f68660d520d49bacca72aa05
shheo <vehumet@gmail.com>
2016-05-23 12:24:39 +0900

    Rate us 앱스토어 링크 추가 (테스트필요)

--- Assets/01_Scripts/30_Gameover/BannerButtons/RateUsBannerButton.cs
+++ Assets/01_Scripts/30_Gameover/BannerButtons/RateUsBannerButton.cs
@@ -6,7 +6,11 @@ public class RateUsBannerButton : BannerButton {
   public int showAfterGames = 5;
 
   override public void activateSelf() {
+#if UNITY_ANDROID
     Application.OpenURL("market://details?id=com.morogoro.smashytoysspace");
+#elif UNITY_IOS
+    Application.OpenURL ("https://itunes.apple.com/app/id1113368167");
+#endif
 
     GetComponent<MeshRenderer>().enabled = false;
     GetComponent<Collider>().enabled = false;
